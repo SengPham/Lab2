007fc4c65d5b37808a1f3e4ce170987c300b52a5
SengPham <101377646+SengPham@users.noreply.github.com>
2022-03-11 10:35:29 +0700

    Add files via upload

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool f = false;
+            while (f = true)
+            {
+                Console.WriteLine("Hello Sang =))");
+            }
+        }
+    }
+}
